caabc0496c71d0bfb8a3ee9d5ec066a23b034eea
adnanakanda <a.akanda@a1qa.com>
2023-12-19 16:02:44 +0600

    initial commit

--- Selenium Final with Accuweather/Selenium/AccweatherTest.cs
+++ Selenium Final with Accuweather/Selenium/AccweatherTest.cs
@@ -12,8 +12,10 @@ namespace Selenium_Final_with_Accuweather.Selenium
 {
     internal class AccweatherTest: BaseTest
     {
+        private static readonly By iUnderstandBtn = By.XPath(string.Format(preciseTextXpath,"I Understand"));
         private static readonly By searchInput = By.CssSelector("input.search-input[name='query']");
-        private static readonly By firstSearchResult = By.XPath(string.Format(preciseTextXpath, "New York"));
+        private static readonly By resultContainer = By.XPath("//div[@class='results-container']");
+        private static readonly By firstSearchResult = By.XPath("//div[@class='search-bar-result search-result source-radar']");
         private static readonly By locationHeader = By.XPath("//h1[contains(@class, 'header-loc')]");
         private static readonly string locationToSearch = "New York";
 
@@ -21,11 +23,9 @@ namespace Selenium_Final_with_Accuweather.Selenium
         [Test]
         public void AccweatherWebTest()
         {
-            // consent data usage
-            if (IsAlertPresent())
-            {
-                driver.SwitchTo().Alert().Accept();
-            }
+            // accept cookies
+            IWebElement acceptButton = driver.FindElement(iUnderstandBtn);
+            acceptButton.Click();
 
             // Input "New York" in the Search field
             IWebElement searchInputField = driver.FindElement(searchInput);
@@ -33,25 +33,16 @@ namespace Selenium_Final_with_Accuweather.Selenium
             searchInputField.SendKeys(locationToSearch);
 
             // Click on the first search result.
-            IWebElement searchResult = wait.Until(ExpectedConditions.ElementIsVisible(firstSearchResult));
-            searchResult.Click();
+            IWebElement firstResultContainer = wait.Until(ExpectedConditions.ElementIsVisible(resultContainer));
+            IWebElement firstSearchResultElement = firstResultContainer.FindElement(firstSearchResult);
+            firstSearchResultElement.Click();
+
+
 
             //Check the whether City weather page header contains city name from the search.
             IWebElement cityWeatherHeader = wait.Until(ExpectedConditions.ElementIsVisible(locationHeader));
             Assert.IsTrue(cityWeatherHeader.Text.Contains(locationToSearch), $"City name in header does not match: Expected {locationToSearch}, Actual {cityWeatherHeader.Text}");
 
-        }
-        private bool IsAlertPresent()
-        {
-            try
-            {
-                driver.SwitchTo().Alert();
-                return true; 
-            }
-            catch (NoAlertPresentException)
-            {
-                return false;
-            }
         }
     }
 }
